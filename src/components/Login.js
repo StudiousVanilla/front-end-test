48dcaa04d7c257d23fc1e6eb8a2bfa02b50d52f4
Oisín_windows <byrne.ois@gmail.com>
2021-02-16 16:22:20 +0000

    a

--- src/components/Login.js
+++ src/components/Login.js
@@ -24,7 +24,7 @@ const Login = (props) => {
     const postData = async (user) => {
         
         try {
-            fetch('http://localhost:3000/login', {
+            fetch('https://oisin-mern-blog.herokuapp.com/login', {
                 method: 'POST',
                 headers: {
                     Accept: 'application/json',
